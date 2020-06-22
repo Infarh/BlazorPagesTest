ff88c99272c5054adfbf28100566be2bffee0c59
Infarh <shmachilin@yandex.ru>
2020-06-22 18:05:00 +0300

    Добавьте файлы проекта.

---
+++ BlazorPagesTest/Data/TestData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BlazorPagesTest.Models;
+
+namespace BlazorPagesTest.Data
+{
+    public static class TestData
+    {
+        public static ICollection<Student> Students { get; } = Enumerable.Range(1, 10)
+           .Select(i => new Student
+           {
+               Name = $"Name-{i}",
+               Surname = $"Surname-{i}",
+               Patronymic = $"Patronymic-{i}",
+               Birthday = DateTime.Now.Subtract(TimeSpan.FromDays(260 * (i + 18)))
+           })
+           .ToList();
+    }
+}
